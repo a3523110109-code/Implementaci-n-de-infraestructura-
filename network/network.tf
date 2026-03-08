107c6a8eddf290bbe26a8f3051952d97b0f6f602
a3523110109-code <a3523110109@alumno.uttehuacan.edu.mx>
2026-03-08 02:04:32 -0600

    network.tf

---
+++ network/network.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "laboratorio_vpc" {
+
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "laboratorio-vpc"
+  }
+
+}
+
+resource "aws_subnet" "laboratorio_subnet" {
+
+  vpc_id     = aws_vpc.laboratorio_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "laboratorio-subnet"
+  }
+
+}
