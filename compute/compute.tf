540ec267519adb3a7f61d70b907536a5ca38bd2d
Lucas Agustin Martinez <a3523110109@alumno.uttehuacan.edu.mx>
2026-03-15 22:26:25 -0600

    Add EC2 tags without terraform providers

--- compute/compute.tf
+++ compute/compute.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "backend_server" {
 
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "backend-laboratorio"
+    Environment = "Development"
+    Owner = "Team"
   }
 
 }
