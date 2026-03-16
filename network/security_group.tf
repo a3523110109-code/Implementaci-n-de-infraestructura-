8a254be9184e46055e4f5842b65ebbc1b88e16e3
Lucas Agustin Martinez <a3523110109@alumno.uttehuacan.edu.mx>
2026-03-15 22:35:51 -0600

    Add security group without terraform providers

---
+++ network/security_group.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
+}
